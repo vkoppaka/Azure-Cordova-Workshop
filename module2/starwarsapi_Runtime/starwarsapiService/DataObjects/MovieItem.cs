4f0c09c98ed444245d99c72611c8c0ffa85a5def
Venkata Koppaka <subbarao.cse@gmail.com>
2015-11-06 19:11:46 -0800

    Azure Mobile API Runtime for StarWars API

---
+++ module2/starwarsapi_Runtime/starwarsapiService/DataObjects/MovieItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Azure.Mobile.Server;
+
+namespace starwarsapiService.DataObjects
+{
+    public class MovieItem : EntityData
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ImageUrl { get; set; }
+        public int SortOrder { get; set; }
+        public string Episode { get; set; }
+    }
+}
